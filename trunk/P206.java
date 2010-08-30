5405432c0e229c3a0802e4602584aa1ee7f5c586
charles.wangkai <charles.wangkai@48939d3b-ad60-1b83-dd4a-19dd7681432f>
2010-08-30 16:44:34 +0000

    P206

--- trunk/P206.java
+++ trunk/P206.java
@@ -1,45 +1,20 @@
 public class P206 {
 	public static void main(String args[]) {
-		long base = 1020304050607080900L;
-		for (int a1 = 0; a1 <= 9; a1++) {
-			for (int a2 = 0; a2 <= 9; a2++) {
-				for (int a3 = 0; a3 <= 9; a3++) {
-					for (int a4 = 0; a4 <= 9; a4++) {
-						for (int a5 = 0; a5 <= 9; a5++) {
-							for (int a6 = 0; a6 <= 9; a6++) {
-								for (int a7 = 0; a7 <= 9; a7++) {
-									for (int a8 = 0; a8 <= 9; a8++) {
-										for (int a9 = 0; a9 <= 9; a9++) {
-											long number = base + a1
-													* 100000000000000000L + a2
-													* 1000000000000000L + a3
-													* 10000000000000L + a4
-													* 100000000000L + a5
-													* 1000000000L + a6
-													* 10000000L + a7 * 100000L
-													+ a8 * 1000L + a9 * 10L;
-											long root;
-											if ((root = isSquare(number)) > 0) {
-												System.out.println(root);
-												System.exit(0);
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
+		for (long i = 1000000000; i * i < 2000000000000000000L; i += 10) {
+			long number = i * i;
+			if (isValid(number + "")) {
+				System.out.println(i);
+				break;
 			}
 		}
 	}
 
-	static long isSquare(long number) {
-		long root = Math.round(Math.sqrt(number));
-		if (root * root == number) {
-			return root;
-		} else {
-			return -1;
+	static boolean isValid(String str) {
+		for (int i = 0; i < str.length(); i += 2) {
+			if (str.charAt(i) - '0' != (i / 2 + 1) % 10) {
+				return false;
+			}
 		}
+		return true;
 	}
 }
